f16fa8da88795a8c5362fb5335fe859eafe111c8
Afonso Lugo <54476092+AfonsoLugo@users.noreply.github.com>
2020-12-09 10:35:46 +0200

    fig1

--- Operational_Analysis_v00.py
+++ Operational_Analysis_v00.py
@@ -61,7 +61,7 @@ option3 = st.selectbox(
 
 ### PLOT DO GRÁFICO ###
 fig1 = plt.figure(figsize = (12,6))
-ax1 = fig.add_subplot(111)
+ax1 = fig1.add_subplot(111)
 
 horario = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24']
 
